dbfc47abed112fab4e769fb5b0d010511536f61d
aslak <s331378@oslomet.no>
2020-10-06 15:12:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/s331389_s331378_mappe2_lemoete/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.s331389_s331378_mappe2_lemoete;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
